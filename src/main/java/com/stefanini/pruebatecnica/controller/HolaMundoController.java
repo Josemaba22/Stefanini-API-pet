18948733fdfc2f9835bf9367602827d9d8f8082b
josemaba22 <josemaba_22@hotmail.com>
2026-06-27 17:23:39 -0600

    feat: estructura de carpetas, arquitectura en capas

---
+++ src/main/java/com/stefanini/pruebatecnica/controller/HolaMundoController.java
@@ -0,0 +1,14 @@
+package com.stefanini.pruebatecnica.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hola")
+public class HolaMundoController {
+  @GetMapping("")
+  public String hello() {
+    return "Hello World! que show";
+  }
+}
