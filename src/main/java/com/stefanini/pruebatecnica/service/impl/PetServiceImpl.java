249b25b98f37e5478e8cda9e2d866c225ea9c124
josemaba22 <josemaba_22@hotmail.com>
2026-06-27 22:41:01 -0600

    feat: se implementa log en consola en PetServiceImpl para el endpoint pet/client/{petId}

--- src/main/java/com/stefanini/pruebatecnica/service/impl/PetServiceImpl.java
+++ src/main/java/com/stefanini/pruebatecnica/service/impl/PetServiceImpl.java
@@ -3,8 +3,10 @@ package com.stefanini.pruebatecnica.service.impl;
 import com.stefanini.pruebatecnica.clients.PetClient;
 import com.stefanini.pruebatecnica.dto.PetResponse;
 import com.stefanini.pruebatecnica.service.PetService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class PetServiceImpl implements PetService {
   private PetClient petClient;
@@ -14,6 +16,8 @@ public class PetServiceImpl implements PetService {
   }
 
   public PetResponse findById(Long petId) {
+    PetResponse petResponse = petClient.getPet(petId);
+    log.info(petResponse.toString());
     return petClient.getPet(petId);
   }
 }
